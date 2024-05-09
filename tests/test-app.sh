ea8c26150f58acbcd6f62b5964dc7cc1c9eaeaa1
AC <ac@marketmining.com.br>
2024-05-09 12:38:45 -0300

    Setup docker

---
+++ tests/test-app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Testing"
+
+RESULT="`wget -qO- http://localhost:8090`"
+wget -q localhost:8090
+
+if [$? -eq o]
+then
+    echo "service running"
+elif [[$RESULT == *"running"*]]
+then 
+    echo "app is runing"
+    echo $RESULT
+else
+    echo "NOT OK"
+    exit 1
+fi
+    
+
